093ae99b93ba48077adef256495d773b566eceec
DavidHsu <davidmd9830415@gmail.com>
2018-01-10 02:26:12 +0800

    update
    修正註解

--- polygon-MTV/SAT-Collision.js
+++ polygon-MTV/SAT-Collision.js
@@ -21,23 +21,38 @@ function SAT_Collision(polygonA, polygonB) {// 以polygonA為基準
         isSeparated = (minMax_B.min > minMax_A.max || minMax_A.min > minMax_B.max);
         if (isSeparated) break;
 
-        // 在A物體的軸上跟在B物體軸上的運算overlap會差一個負號
+        // 因為我們需要的是兩物體間的最短推出距離，但只取一個D1的話可能取到兩個最遠的角落
+        /*
+        如以下範例 * 四個點為兩物體的最大最小投影
+
+        *(a.min)      *(b.min)          *(a.max)          *(b.max)
+                       <-----------------> is d1
+        <--------------------------------------------------------> is d2
+        we need d1, not d2
+        */
         let d1 = minMax_A.max - minMax_B.min;
         let d2 = minMax_B.max - minMax_A.min;
         let overlap = d1 < d2 ? d1 : d2;
+        // 在A物體的軸上跟在B物體軸上的運算overlap會差一個負號
         if (Math.abs(overlap) < MTV.length()) {
             let n = normals[i].clone().norm();
             MTV = n.multiplyScalar(overlap);
         }
     }
     // 在最後判斷這個是以哪個物體的分離軸為準，如果dot值為負代表方向相反了
-    // 因為D的意思是A-B的向量(以A為基準)
-    // 當物體B撞到A時，要有一個反向的MTV將它推回去，這就代表他的方向一定不會是往A
+    // 因為D的意思是向量AB(以A為基準)
+
+    // 當物體B撞到A時，要有一個反向的MTV將它推回去，但有時MTV的方向會跟向量AB同方向，導致B物體反而往A裡面推
     // 這就是為什麼用dot來判斷是否相反
+
+    // 回傳的MTV是polygonB要移動的向量，所以當作用力的方向與B與A的相對位置不一致時，需做反向
+    // 正確的MTV是跟向量AB同方向，這樣才能正確地把B以最短距離推出A的範圍
+    // vec(B-A) = vecAB
     let D = new Vector(polygonB.pos.x - polygonA.pos.x,polygonB.pos.y - polygonA.pos.y);
     if(MTV.dot(D) < 0) MTV.multiplyScalar(-1);
 
     // isSeparated = true:Separated boxes, false:Collided boxes
+    
     return { isCollided: !isSeparated, mtv: MTV };
 }
 
